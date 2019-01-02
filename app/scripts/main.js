4ac57ce5baf4b743d26db0405c60b8cd05be6ce2
Ali Briceño <alienriquebm@gmail.com>
2019-01-02 16:42:40 -0400

    finnished effect of paralax

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,10 @@
 console.log('\'Allo \'Allo!');
+$(window).scroll(function () {
+  const scrollValue = $(this).scrollTop();
+  $('.main-logo').css('transform', `translate(0px, ${scrollValue / 2}%`);
+  $('.main-clouds1').css('transform', `translate(0px, -${scrollValue / 15}%`);
+  $('.main-clouds2').css('transform', `translate(0px, -${scrollValue / 55}%`);
+  if(scrollValue >= window.innerHeight) {
+    console.log("Llegue al máximo")
+  }
+})
