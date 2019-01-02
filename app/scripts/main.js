5e2f1a77cdf4e2dac6c456be694e49647aabb616
Ali Briceño <alienriquebm@gmail.com>
2019-01-02 19:34:59 -0400

    added bootstrap and annimation of landing

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,7 +4,13 @@ $(window).scroll(function () {
   $('.main-logo').css('transform', `translate(0px, ${scrollValue / 2}%`);
   $('.main-clouds1').css('transform', `translate(0px, -${scrollValue / 15}%`);
   $('.main-clouds2').css('transform', `translate(0px, -${scrollValue / 55}%`);
-  if(scrollValue >= window.innerHeight) {
-    console.log("Llegue al máximo")
+  $('.main-clouds3').css('transform', `translate(0px, -${scrollValue / 35}%`);
+  $('.main-clouds4').css('transform', `translate(0px, -${scrollValue / 25}%`);
+  if (scrollValue >= window.innerHeight) {
+    $('.navbar').css('position', 'fixed');
+    $('.navbar').css('top', 0);
+  } else {
+    $('.navbar').css('position', 'initial');
+    $('.navbar').css('top', 'initial');
   }
 })
