c9b03fa15f5b446a7fe04d398fb471b146e67b66
Ali Briceño <alienriquebm@gmail.com>
2019-01-05 19:09:09 -0400

    added feed behance

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -23,6 +23,8 @@ $('.carousel').carousel({
   interval: 1000 * 7,
 });
 
+// Instagram Access
+
 const token = '2297818537.4a9aa16.f9fa6d84db9044189add40af5cf7886e'; // learn how to obtain it below
 // const userid = 'sanbricdesigns'; // User ID - get it in source HTML of your Instagram profile
 const numPhotos = 8; // how much photos do you want to get
@@ -53,3 +55,34 @@ const numPhotos = 8; // how much photos do you want to get
   },
 });
  */
+
+// Behance Access
+const apiKey = 'eo7VzCzOBa5x6OsDnWH0dqN0FkBFYuKd';
+const collection = '170537937';
+
+$.ajax({
+  url: `http://behance.net/v2/collections/${collection}/projects?api_key=${apiKey}`, // or /users/self/media/recent for Sandbox
+  dataType: 'jsonp',
+  type: 'GET',
+  // data: { access_token: token, count: numPhotos },
+  success: (response) => {
+    console.log(response);
+    const { projects } = response;
+    $('#loader-behance').hide();
+    for (let i = 0; i < projects.length; i += 1) {
+      const project = projects[i];
+      $('.behance-feed').append(`
+      <div class="behance-feed-project">
+        <img src="${project.covers[404]}"></img>
+        <div class="behance-feed-project-title text-center">
+        <div class="behance-feed-project-stats">
+        ${project.name}
+        </div>
+      </div>
+        `);
+    }
+  },
+  error: (data) => {
+    console.log(data); // send the error notifications to console
+  },
+});
