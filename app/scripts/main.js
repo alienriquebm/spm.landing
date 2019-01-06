952b801a7530d322fa8e1dff7421d0db573bb54d
Ali Briceño <alienriquebm@gmail.com>
2019-01-05 23:20:47 -0400

    added form contact

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -101,3 +101,63 @@ $.ajax({
     console.log(data); // send the error notifications to console
   },
 });
+
+
+grecaptcha.ready(() => { // eslint-disable-line
+  $('#contact-form-button').attr('disabled', false);
+  grecaptcha.execute('6Lf4KIcUAAAAAJJLHVglp-0qOxL_YuNExFgZ9ath', { action: 'contactus' }) // eslint-disable-line
+    .then((token) => {
+      // Verify the token on the server.
+      console.log(token);
+    });
+});
+
+
+// Mail
+
+$(() => {
+  // Get the form.
+  const form = $('#contact-form');
+
+  // Get the messages div.
+  const formMessages = $('.form-message');
+
+  // Set up an event listener for the contact form.
+  $(form).submit((e) => {
+    // Stop the browser from submitting the form.
+    e.preventDefault();
+
+    // Serialize the form data.
+    const formData = $(form).serialize();
+
+    // Submit the form using AJAX.
+    $.ajax({
+      type: 'POST',
+      url: $(form).attr('action'),
+      data: formData,
+    })
+      .done((response) => {
+        // Make sure that the formMessages div has the 'success' class.
+        $(formMessages).removeClass('error');
+        $(formMessages).addClass('success');
+
+        // Set the message text.
+        $(formMessages).text(response);
+
+        // Clear the form.
+        $('#contact-form input,#contact-form textarea').val('');
+      })
+      .fail((data) => {
+        // Make sure that the formMessages div has the 'error' class.
+        $(formMessages).removeClass('success');
+        $(formMessages).addClass('error');
+
+        // Set the message text.
+        if (data.responseText !== '') {
+          $(formMessages).text(data.responseText);
+        } else {
+          $(formMessages).text('Oops! An error occured and your message could not be sent.');
+        }
+      });
+  });
+});
