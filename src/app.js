4fcd4707753523de3d308593d9dc6c71dae1f01d
Ali Briceño <alienriquebm@gmail.com>
2019-01-10 15:36:04 -0400

    reset of project added webpack and webpack dev with sample code

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const button = document.querySelector('#test');
+const p = document.querySelector('#secret');
+
+let value = true;
+
+button.addEventListener('click', () => {
+  if (value) {
+    p.classList.add('show');
+    p.classList.remove('hide');
+    button.innerHTML = "Hide";
+  } else {
+    p.classList.add('hide');
+    p.classList.remove('show');
+    button.innerHTML = "Show";
+  }
+  value = !value;
+});
