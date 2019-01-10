2e60c3cd632d4f856337da3df9afd8559794e27c
Ali Briceño <alienriquebm@gmail.com>
2019-01-10 15:55:31 -0400

    added entry and output points to webpack

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'), // Absolute path for output
+    filename: 'bundle.js', // Name of the generated js final file
+    publicPath: '/dist', // To tell the web dev server where to look the bundle
+  },
+};
