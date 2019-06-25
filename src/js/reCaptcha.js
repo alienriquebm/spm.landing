1cfe8fe2d1f2092a2626146ac62e83f3747fada9
Ali Briceño <alienriquebm@gmail.com>
2019-06-24 20:12:19 -0400

    load dinamically recaptcha script

--- src/js/reCaptcha.js
+++ src/js/reCaptcha.js
@@ -1,9 +1,17 @@
 // Libraries
 import $ from 'jquery';
 
-if (window.grecaptcha) {
-  window.grecaptcha.ready(() => { // eslint-disable-line
-    $('#contact-form-button').attr('disabled', false);
-    $('#contact-form-button').text('Send');
-  });
-}
+// Load reCaptcha script dinamically
+const head = document.getElementsByTagName('head')[0];
+const script = document.createElement('script');
+script.type = 'text/javascript';
+script.onload = () => {
+  if (window.grecaptcha) {
+    window.grecaptcha.ready(() => { // eslint-disable-line
+      $('#contact-form-button').attr('disabled', false);
+      $('#contact-form-button').text('Send');
+    });
+  }
+};
+script.src = `https://www.google.com/recaptcha/api.js?render=${process.env.RECAPTCHA_SECRET}`;
+head.appendChild(script);
